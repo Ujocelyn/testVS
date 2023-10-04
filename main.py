c0a24a52d30086ebcea5df056a5b45cf95b539b3
Uribe <juribe13@cps.k12.il.us>
2023-10-04 08:54:01 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,5 @@
+#data abstraction when you need to simplify data
+#from something more complex 
+#and example of data abstraction is when you use a list to implufy data
+#instead of having to writte out the same code over and over again
+#EX: Have a multiple 
